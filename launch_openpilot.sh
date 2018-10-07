19e08c20aa510fa8107f216f42de0d4e0c819da9
Chris Souers <csouers@gmail.com>
2018-10-06 20:58:31 -0400

    default to OP if not up

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 #export PASSIVE="0"
-export PASSIVE=$(( $(cat /sys/class/switch/tri-state-key/state) == 1 ? 1 : 0 ))
+export PASSIVE=$(( $(cat /sys/class/switch/tri-state-key/state) != 1 ? 1 : 0 ))
 exec ./launch_chffrplus.sh
